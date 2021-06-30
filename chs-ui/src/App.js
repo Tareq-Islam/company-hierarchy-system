8824f036c7f864d6bba1010f73ee8509e1f8e192
Tareq Islam <tareqislamidb@gmail.com>
2021-06-30 21:06:52 +0600

    add new feature

--- chs-ui/src/App.js
+++ chs-ui/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch
+} from "react-router-dom";
+import Department from "./Department";
+import Employee from "./Employee";
+import Home from "./Home";
+import Nav from "./nav";
+import Role from "./Role";
 
 function App() {
   return (
-    <div className="App">
-      Reac application
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>  
+        <Switch>
+          <Route path="/employee">
+            <Employee />
+          </Route>
+          <Route path="/department">
+            <Department />
+          </Route>
+          <Route path="/role">
+            <Role />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+     </div>  
+    </Router>    
   );
 }
 
